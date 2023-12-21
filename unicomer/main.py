c69897d7803f001c774fcb7f6179220ac63d65d5
Alexander Merino <barcealex1014@hotmail.com>
2023-12-21 15:57:03 -0500

    add new features

--- unicomer/main.py
+++ unicomer/main.py
@@ -1,8 +1,52 @@
-from fastapi import FastAPI
-
+from fastapi import FastAPI, File, UploadFile
+from google.cloud import documentai_v1beta3 as documentai
+from google.protobuf.json_format import MessageToDict
+from fastapi.middleware.cors import CORSMiddleware
+import json
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
-@app.get("/mi-recurso")
+documentai_client = documentai.DocumentProcessorServiceClient()
+
+@app.get("/api/v1/document/test")
 def read_root():
-    return {"mensaje": "¡Hola, mundo!"}
+    return {"mensaje": "API REST 1.0 UNICOMER"}
+
+
+@app.post("/api/v1/document/analizate")
+async def analizar_documento(file: UploadFile = File(...)):
+    content = await file.read()
+    mime_type = file.content_type
+    result = await parse_form_async(content, mime_type)
+
+    return result
+
+
+async def parse_form_async(content, mime_type):
+    request = {
+        'name': 'projects/{}/locations/us/processors/c2388ff2df8c3cf'.format('arch-dataloader-poc'),
+        'raw_document': {'content': content, 'mime_type': mime_type},
+    }
+
+    try:
+        response = documentai_client.process_document(request=request)
+        response_dict = MessageToDict(response._pb)
+        data=''
+        if 'document' in response_dict:
+            document_value = response_dict['document']
+            print(document_value)
+            if 'text' in document_value:
+                text_value = document_value['text']
+                print(f"Valor del campo 'text': {text_value}")
+                data = text_value
+        return {"message": "Formulario analizado exitosamente","data": data}
+    except Exception as e:
+        print(e)
+        return {"error": str(e)}
